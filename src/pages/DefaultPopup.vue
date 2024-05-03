1a806c4e3f32535b092efae81c45ec459c4b59d2
neyasbltb88 <neyasbltb_88@mail.ru>
2024-05-03 04:43:32 +0300

    В DefaultPopup для теста выводятся MiniBtn и ListItem

--- src/pages/DefaultPopup.vue
+++ src/pages/DefaultPopup.vue
@@ -1,11 +1,40 @@
 <template>
-    <div class="DefaultPopup">DefaultPopup</div>
+    <div class="DefaultPopup">
+        DefaultPopup:
+
+        <!-- FIXME: На V3 дважды вызывается обработчик -->
+        <MiniBtn @click="onClick" />
+
+        <ListItem
+            :item="item"
+            controlType="add"
+            dense
+            clickable
+            hovered
+            selectable
+            selectOnItemClick
+            controlBaseColor="ms-theme-red"
+        />
+    </div>
 </template>
 
 <script setup lang="ts">
+import ListItem from '@components/ListItem';
+import MiniBtn from '@components/MiniBtn';
+
 defineOptions({
     name: 'DefaultPopup'
 });
+
+const item = {
+    value: 'example',
+    label: 'Default args',
+    icon: 'cancel'
+};
+
+const onClick = (e: MouseEvent) => {
+    console.log('onClick: ', e);
+};
 </script>
 
 <style lang="sass">
