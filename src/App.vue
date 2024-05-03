9134dacb7961997dff98ab349eca0f0e9e13a483
neyasbltb88 <neyasbltb_88@mail.ru>
2024-05-03 04:43:02 +0300

    В компоненте App в window пробрасываются все утилиты

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,20 @@
 </template>
 
 <script setup lang="ts">
+import objectForEach from '@utils/objectForEach';
+
+const passingUtilsToWindow = (utils: Record<string, Function>) => {
+    objectForEach(utils, (fn, name) => {
+        // @ts-ignore
+        window['_' + name] = fn;
+    });
+};
+
+import * as utils from '@utils';
+// @ts-ignore
+window.utils = utils;
+passingUtilsToWindow(utils);
+
 defineOptions({
     name: 'App'
 });
